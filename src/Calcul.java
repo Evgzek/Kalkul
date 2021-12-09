9d09bea80bf8a526145b6f4dea8128668faed137
Evgzek <zekamusijhcuk@gmail.com>
2021-12-09 11:36:59 +1000

    Прописываем что будет участвовать в программе: два числа, оператор, результат

--- src/Calcul.java
+++ src/Calcul.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
 public class Calcul {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result;
+        char operator;
+
+    }
 }
